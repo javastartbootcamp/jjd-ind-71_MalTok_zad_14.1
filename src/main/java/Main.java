e0a539111a52d0a1db5ae23ac4a644af8a2cc893
MalTok <mig01@wp.pl>
2023-08-24 19:24:09 +0200

    Rozwiązanie zadania

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Main {
 
-
     public static void main(String[] args) {
-
+        String fileName = "liczby.txt";
+        try (
+                Scanner scanner = new Scanner(new File(fileName))
+        ) {
+            NumberCounter numberCounter = new NumberCounter();
+            List<Integer> listFromFile = numberCounter.createListFromFile(scanner);
+            Map<Integer, Integer> numbersMap = numberCounter.createMap(listFromFile);
+            Set<Map.Entry<Integer, Integer>> entries = numbersMap.entrySet();
+            showResult(entries);
+        } catch (FileNotFoundException e) {
+            System.err.println("Nie odnaleziono pliku " + fileName);
+        }
     }
 
+    private static void showResult(Set<Map.Entry<Integer, Integer>> entries) {
+        for (Map.Entry<Integer, Integer> entry : entries) {
+            System.out.printf("%3d - liczba wystąpień %d\n", entry.getKey(), entry.getValue());
+        }
+    }
 }
